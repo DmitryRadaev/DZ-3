810bb4dfebce9f92b5c4a6f9785857571d404bf2
Dmitry Radaev <RadaevDM@mail.ru>
2023-04-26 22:30:44 +0500

    add code

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
 
+        int x = 200; // денег на счету клиента
+        int y = 1300; // клиент положил на счёт
+        int v = y / 100;
+        System.out.printf("Итоговый баланс =" + " " + (v + y + x) + " " + "рублей." + " ");
 
-      
+        if (y > 1000) {
 
-
+            System.out.printf("Начислено бонусных рублей" + " " + v + ".");
 
         }
+
+
     }
 }
